ca18bb2c51bd8d5842ebd3c6cd1d9daeafde07d7
cfs <sidk89329@gmail.com>
2025-11-27 04:38:40 +0530

    Update production API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,13 @@
 
-let API_URL = "https://slooze-rust-backend.vercel.app/";
+let API_URL = "https://slooze-rust-backend.up.railway.app";
 
 if (process.env.NODE_ENV === "development") {
     API_URL = "http://localhost:4000";
 }
 
 export const api = {
-    getProducts: () => fetch(`${API_URL}/cfs-api`).then(r => r.json()),
-    addProduct: (data: any) => fetch(`${API_URL}/cfs-api`, { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } }).then(r => r.json()),
-    updateProduct: (id: number, data: any) => fetch(`${API_URL}/${id}/cfs-api`, { method: "PUT", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } }).then(r => r.json()),
-    deleteProduct: (id: number) => fetch(`${API_URL}/${id}/cfs-api`, { method: "DELETE" })
+    getProducts: () => fetch(`${API_URL}`).then(r => r.json()),
+    addProduct: (data: any) => fetch(`${API_URL}`, { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } }).then(r => r.json()),
+    updateProduct: (id: number, data: any) => fetch(`${API_URL}/${id}`, { method: "PUT", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } }).then(r => r.json()),
+    deleteProduct: (id: number) => fetch(`${API_URL}/${id}`, { method: "DELETE" })
 };
